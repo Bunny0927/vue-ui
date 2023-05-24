f47c3038112858742c091308e53d7bd008f9032f
Bunny0927 <1315212328@qq.com>
2023-05-24 21:39:59 +0800

    update dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import Vue from 'vue';
 import { Button } from "./components/Button";
 Vue.config.productionTip = false;
 
-const install = function (Vue, options = {}) {
+// const install = function (Vue, options = {}) {
+    const install = function (Vue) {  
     Vue.use(Button);
 };
 
