0bc8d7b3e7d86bebd84ae0cd5878cded1fbe8bd6
Bunny0927 <1315212328@qq.com>
2023-08-17 16:22:26 +0800

    style：增加样式文件

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import { Input } from "./components/Input";
 import { Form } from "./components/Form";
 import { Switcher } from "./components/Switch";
 import { Message } from './components/Message';
+// import Message from "./components/Message";
+import {Radio,RadioButton,RadioGroup} from "./components/Radio";
+import './assets/iconfont/iconfont.css';
+// import './styles/index.scss';
 Vue.config.productionTip = false;
 
 const install = function (Vue) {  
@@ -15,6 +19,23 @@ const install = function (Vue) {
     Vue.use(Form);
     Vue.use(Switcher);
     Vue.use(Message);
+    Vue.use(Radio);
+    Vue.use(RadioButton);
+    Vue.use(RadioGroup);
+    // Vue.prototype.$notice = Message;
+
 };
 
 export default install;
+
+export {
+    Button,
+    Dialog,
+    Input,
+    Form,
+    Switcher,
+    Message,
+    Radio,
+    RadioButton,
+    RadioGroup
+};
